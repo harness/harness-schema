55ed8364e570590f42519abf52a6367d4458b0dc
utkarshchoubey-harness <utkarsh.choubey@harness.io>
2023-06-27 15:21:38 +0530

    feat: [CDS-73107]: Added Schema for Stepgroup template monitered service template

--- bundler/schema_store/src/main/java/io/harness/SchemaBundleUtils.java
+++ bundler/schema_store/src/main/java/io/harness/SchemaBundleUtils.java
@@ -34,6 +34,7 @@ public class SchemaBundleUtils implements SchemaBundler {
   public final String NAME = "name";
   public final String ORG_IDENTIFIER = "orgIdentifier";
   public final String PROJECT_IDENTIFIER = "projectIdentifier";
+  public final String TYPE = "type";
   private static final ObjectMapper YAML_OBJECT_MAPPER = new ObjectMapper(new YAMLFactory());
   private static final ObjectMapper JSON_OBJECT_MAPPER = new ObjectMapper();
   private static final String REF_NODE = "$ref";
@@ -134,8 +135,7 @@ public class SchemaBundleUtils implements SchemaBundler {
   private void updateJsonForTemplate(JsonNode childSchemaNode, String refValue) {
     String title = childSchemaNode.get(TITLE).asText();
     String newTitle = title + "_template";
-    HashSet<String> keys =
-        new HashSet<>(Arrays.asList(NAME, IDENTIFIER, DESCRIPTION, ORG_IDENTIFIER, PROJECT_IDENTIFIER));
+    HashSet<String> keys = getKeysToRemoveFromTemplateSpec(refValue);
     if (refValue.contains("../pipeline/")) {
       deletePropertiesInJsonNode((ObjectNode) childSchemaNode.get("properties"), keys);
       deletePropertiesInArrayNode((ArrayNode) childSchemaNode.get("required"), keys);
@@ -143,6 +143,17 @@ public class SchemaBundleUtils implements SchemaBundler {
       propertiesToUpdate.put(TITLE, newTitle);
       updatePropertiesInJsonNode((ObjectNode) childSchemaNode, propertiesToUpdate);
     }
+  }
+
+  private  HashSet<String> getKeysToRemoveFromTemplateSpec(String refValue) {
+
+    if (refValue.contains("step-group-element-config.yaml")) {
+      return new HashSet<>(Arrays.asList(NAME, IDENTIFIER, DESCRIPTION, ORG_IDENTIFIER, PROJECT_IDENTIFIER, TYPE));
+    } else if (refValue.contains("stage-node.yaml") || refValue.contains("step-node.yaml")) {
+      return new HashSet<>(Arrays.asList(NAME, IDENTIFIER, DESCRIPTION, ORG_IDENTIFIER, PROJECT_IDENTIFIER));
+    }
+
+    return new HashSet<>();
   }
 
   public static JsonNode deletePropertiesInJsonNode(ObjectNode jsonNode, Collection<String> properties) {
