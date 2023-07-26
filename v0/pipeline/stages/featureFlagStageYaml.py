8b33ce0cee6b5ad14f0121dff5886529ad86746d
Prashant Sharma <prashant.sharma@harness.io>
2023-07-26 13:33:39 +0530

    save changes

--- v0/pipeline/stages/featureFlagStageYaml.py
+++ v0/pipeline/stages/featureFlagStageYaml.py
@@ -345,10 +345,10 @@ def my_function(module,prefix):
 
 
 
-moduleList = ['iacm']
+moduleList = ['cd']
 # moduleList = ['']
 
 for module in moduleList:
 	print()
-	my_function(module,'IACMStageNode')
+	my_function(module,'DeploymentStageNode')
 f.close()
