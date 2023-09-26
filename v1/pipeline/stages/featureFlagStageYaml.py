1ec419328d804a4454b48e2a63d04ff84ad9c8a6
sahithibanda01 <139854962+sahithibanda01@users.noreply.github.com>
2023-09-26 10:09:33 +0530

    fix:[CI-9167]: added static schema for CI steps for V1 version (#134)
    * added static schema for CI steps for V1 version
    
    * updated pipeline.json
    
    * refactored the files
    
    * refactored the files
    
    * modified changes in v1 version
    
    * .
    
    * changes for pr comments
    
    * .
    
    * deleted resource files
    
    * .

--- v1/pipeline/stages/featureFlagStageYaml.py
+++ v1/pipeline/stages/featureFlagStageYaml.py
@@ -2,7 +2,7 @@ import json
 import yaml
 import os
 from yaml.loader import SafeLoader
-f = open('prod.json')
+f = open('../steps/prod.json')
 moduleSuffix = {}
 data = json.load(f)
 duplicateFileName = {}
@@ -48,8 +48,6 @@ def convertFileName(s):
 		return "secret-ng-variable.yaml"
 	elif modified_s == "string-ngvariable.yaml":
 		return "string-ng-variable.yaml"
-	elif modified_s == "prbuild-spec.yaml":
-		return "pr-build-spec.yaml"
 
 	elif modified_s == "azure-armrollback-step-node.yaml":
 		return "azure-a-r-m-rollback-step-node.yaml"
@@ -93,23 +91,58 @@ def convertFileName(s):
 	elif modified_s == "tas-bgapp-setup-step-node.yaml":
 		return "tas-b-g-app-setup-step-node.yaml"
 
+	elif modified_s == "cvngstep-info.yaml":
+		return "cvng-step-info.yaml"
+	elif modified_s == "cvverify-step-node.yaml":
+		return "cv-verify-step-node.yaml"
+
+	elif modified_s == "amiartifact-config.yaml":
+		return "ami-artifact-config.yaml"
+	elif modified_s == "amifilter.yaml":
+		return "ami-filter.yaml"
+	elif modified_s == "amitag.yaml":
+		return "ami-tag.yaml"
+
+	elif modified_s == "custom-deployment-connector-ngvariable.yaml":
+		return "custom-deployment-connector-ng-variable.yaml"
+	elif modified_s == "custom-deployment-ngvariable.yaml":
+		return "custom-deployment-ng-variable.yaml"
+	elif modified_s == "custom-deployment-number-ngvariable.yaml":
+		return "custom-deployment-number-ng-variable.yaml"
+
+
+	elif modified_s == "custom-deployment-secret-ngvariable.yaml":
+		return "custom-deployment-secret-ng-variable.yaml"
+	elif modified_s == "custom-deployment-string-ngvariable.yaml":
+		return "custom-deployment-string-ng-variable.yaml"
+	elif modified_s == "custom-deployment-number-ngvariable.yaml":
+		return "custom-deployment-number-ng-variable.yaml"
+
+
+	elif modified_s == "k8-sdirect-infrastructure.yaml":
+		return "k8-s-direct-infrastructure.yaml"
+	elif modified_s == "ngvariable-override-set-wrapper.yaml":
+		return "ng-variable-override-set-wrapper.yaml"
+	elif modified_s == "ngvariable-override-sets.yaml":
+		return "ng-variable-override-sets.yaml"
+
 
 	return modified_s
+
 
 def fileDirPath(module,expected,newFileNameWithScore):
 	if module==expected:
 		return newFileNameWithScore
 	if (not module or  module == 'pipeline'):
-		if expected == 'pie':
+		if expected == 'common':
 			return newFileNameWithScore
 	return '../' + expected + '/'+newFileNameWithScore
 
 
 def saveRefFile(module, newyaml, fileName):
 	newFileNameWithScore = convertFileName(fileName)
-	print("saveRefFile newyaml[] "+ newFileNameWithScore)
 	if os.path.exists('../common/' + newFileNameWithScore):
-		print("file exists ../../common/"+ newFileNameWithScore)
+		# print("file already there "+ newFileNameWithScore)
 		if(newFileNameWithScore+module in duplicateFileName):
 			#duplicateFileName[fileName+module].append(newyaml)
 			print()
@@ -118,30 +151,33 @@ def saveRefFile(module, newyaml, fileName):
 			duplicateFileName[newFileNameWithScore+module].append(newyaml)
 		return '../../common/' + newFileNameWithScore
 
-	if os.path.exists('common/' + newFileNameWithScore):
-		print("file exists ../common/"+ newFileNameWithScore)
-		print(os.getcwd())
-		if(newFileNameWithScore+module in duplicateFileName):
-			#duplicateFileName[fileName+module].append(newyaml)
-			print()
-		else:
-			duplicateFileName[newFileNameWithScore+module]=[]
-			duplicateFileName[newFileNameWithScore+module].append(newyaml)
-		return '../common/' + newFileNameWithScore
+	elif os.path.exists('../steps/common/' + newFileNameWithScore):
+		return '../../steps/common/' + newFileNameWithScore
 
-	if os.path.exists('custom/' + newFileNameWithScore):
-		# print("file already there "+ newFileNameWithScore)
-		if(newFileNameWithScore+module in duplicateFileName):
-			#duplicateFileName[fileName+module].append(newyaml)
-			print()
-		else:
-			duplicateFileName[newFileNameWithScore+module]=[]
-			duplicateFileName[newFileNameWithScore+module].append(newyaml)
-		return fileDirPath(module,'custom', newFileNameWithScore)
+	elif os.path.exists('../steps/custom/' + newFileNameWithScore):
+		return '../../steps/custom/' + newFileNameWithScore
+
+	elif os.path.exists('../steps/ci/' + newFileNameWithScore):
+		return '../../steps/ci/' + newFileNameWithScore
+	elif os.path.exists('../steps/cvng/' + newFileNameWithScore):
+		return '../../steps/cvng/' + newFileNameWithScore
+
+	elif os.path.exists('../steps/security/' + newFileNameWithScore):
+		return '../../steps/security/' + newFileNameWithScore
+
+	elif os.path.exists('../steps/iacm/' + newFileNameWithScore):
+		return '../../steps/iacm/' + newFileNameWithScore
+
+	elif os.path.exists('../steps/cd/' + newFileNameWithScore):
+		return '../../steps/cd/' + newFileNameWithScore
+
 	else:
 		fileNameWithoutYaml = fileName.removesuffix('.yaml')
 		my_function(module,fileNameWithoutYaml)
 		return fileDirPath(module, module, newFileNameWithScore)
+	# with open(module + '/'+ fileName, 'w') as file:
+	# 	yaml.dump(newyaml, file, sort_keys=False)
+	# 	return './' + module + '/'+fileName
 
 
 def getYamlFromPathList(pathList):
@@ -157,12 +193,10 @@ def getYamlFromPathList(pathList):
 
 def saveYaml(pathList):
 	newyaml = getYamlFromPathList(pathList)
-	if len(pathList)<=3:
+	if len(pathList)<=2:
 		module = 'custom'
 	else:
 		module=pathList[2]
-	print(pathList)
-	print("module name is "+ module)
 	fileName = pathList[len(pathList)-1] + '.yaml'
 	return saveRefFile(module,newyaml,fileName)
 
@@ -175,7 +209,6 @@ def handleListElement(listData):
 			if(listElement == "$ref" and listData[c].startswith("#/")):
 				pathList = listData[c].split('/')
 				savedNewFilePath=saveYaml(pathList)
-				print("prashant element [] "+ listData[c] + " new path " + savedNewFilePath)
 				listData[c]=savedNewFilePath
 				return savedNewFilePath
 		if isinstance(listElement,list):
@@ -190,7 +223,6 @@ def iterateOverMap(yamlData):
 				# print(element)
 				if(element == "$ref" and yamlData[element].startswith("#/")):
 					savedNewFilePath = saveYaml(yamlData[element].split('/'))
-					print("prashant element [] "+ yamlData[element] + " new path " + savedNewFilePath)
 					yamlData[element] = savedNewFilePath
 			#print(element)
 			if isinstance(yamlData[element],dict):
@@ -204,13 +236,11 @@ def read_yaml(path):
 	#print(path)
 	with open(path) as yamlField:
 		yamlData = yaml.load(yamlField, Loader=SafeLoader)
-		print(yamlData)
 		for element in yamlData:
 			if isinstance(element, str):
 				if(element == "$ref" and yamlData[element].startswith("#/")):
-					print("prashant element [] "+ yamlData[element] + " new path " + savedNewFilePath)
+					print("prashant check here - "+ yamlData[element].startswith("#/"))
 					savedNewFilePath = saveYaml(yamlData[element].split('/'))
-					print("prashant element [] "+ yamlData[element] + " new path " + savedNewFilePath)
 					yamlData[element] = savedNewFilePath
 			if isinstance(yamlData[element], dict):
 				iterateOverMap(yamlData[element])
@@ -228,26 +258,22 @@ def updateDescription(updatedYamlData, fileName):
 		properties["description"]={}
 	description = properties["description"]
 	description["desc"]='This is the description for '+fileName
-	print(properties)
 
 def updateYaml(path,yamlContent):
 	with open(path, 'w') as file2:
-		#print(yamlContent)
 		yaml.dump(yamlContent,file2,sort_keys=False)
 		file2.close
 # Iterating through the json
 # list
 def my_function(module,prefix):
 	level = data
-	# print(prefix)
 	if module+prefix in moduleSuffix:
 		return
 	moduleSuffix[module+prefix]=1
-	if(not module or  module == 'pipeline' or module == 'custom'):
-		level = data
+	if(not module or  module == 'pipeline' or module == 'common' or module=='pie'):
+		level = data["pipeline"]
 		directoryPath = 'custom' + '/'
 		if not os.path.exists('custom'):
-			print('created the custom module')
 			os.makedirs('custom')
 	else:
 		level = data[module]
@@ -256,38 +282,45 @@ def my_function(module,prefix):
 			os.makedirs(module)
 	for i in level:
 		if i.endswith(prefix):
+			#py_data = json.loads(data[i])
+			yaml_data = yaml.dump(level[i])
 			fileName = i + '.yaml'
 			fileNameWithUnderscore =convertFileName(fileName)
-			#print(yaml_data)
 			if os.path.exists('../common/' + fileNameWithUnderscore):
-				with open('../common/' + fileNameWithUnderscore, 'w') as file:
-					yaml.dump(level[i], file, sort_keys=False)
-				updatedYamlData = read_yaml('../../common/' + fileNameWithUnderscore)
-				title={"title":fileName.removesuffix('.yaml')}
-				updatedYamlData={**title,**updatedYamlData}
-				updateDescription(updatedYamlData,fileName.removesuffix('.yaml'))
-				updateYaml('common/'+fileNameWithUnderscore,updatedYamlData)
-			elif os.path.exists('../common/' + fileNameWithUnderscore):
-				with open('../common/' + fileNameWithUnderscore, 'w') as file:
-					yaml.dump(level[i], file, sort_keys=False)
+				#create a steps file to compare
 				updatedYamlData = read_yaml('../common/' + fileNameWithUnderscore)
-				title={"title":fileName.removesuffix('.yaml')}
-				updatedYamlData={**title,**updatedYamlData}
-				updateDescription(updatedYamlData,fileName.removesuffix('.yaml'))
 				updateYaml('../common/'+fileNameWithUnderscore,updatedYamlData)
-			elif os.path.exists('custom/' + fileNameWithUnderscore):
-				print("tere")
-				with open('custom/' + fileNameWithUnderscore, 'w') as file:
-					yaml.dump(level[i], file, sort_keys=False)
-				updatedYamlData = read_yaml('custom/' + fileNameWithUnderscore)
-				title={"title":fileName.removesuffix('.yaml')}
-				updatedYamlData={**title,**updatedYamlData}
-				updateDescription(updatedYamlData,fileName.removesuffix('.yaml'))
-				updateYaml('custom/'+fileNameWithUnderscore,updatedYamlData)
-			#print()
+
+			elif os.path.exists('../steps/common/' + fileNameWithUnderscore):
+				return '../../steps/common/' + fileNameWithUnderscore
+
+			elif os.path.exists('../steps/custom/' + fileNameWithUnderscore):
+				updatedYamlData = read_yaml('../steps/custom/' + fileNameWithUnderscore)
+				updateYaml('../steps/custom/'+fileNameWithUnderscore,updatedYamlData)
+
+			elif os.path.exists('../steps/ci/' + fileNameWithUnderscore):
+				updatedYamlData = read_yaml('../steps/ci/' + fileNameWithUnderscore)
+				updateYaml('../steps/ci/'+fileNameWithUnderscore,updatedYamlData)
+
+			elif os.path.exists('../steps/cd/' + fileNameWithUnderscore):
+				updatedYamlData = read_yaml('../steps/cd/' + fileNameWithUnderscore)
+				updateYaml('../steps/cd/'+fileNameWithUnderscore,updatedYamlData)
+
+			elif os.path.exists('../steps/iacm/' + fileNameWithUnderscore):
+				updatedYamlData = read_yaml('../steps/iacm/' + fileNameWithUnderscore)
+				updateYaml('../steps/iacm/'+fileNameWithUnderscore,updatedYamlData)
+
+			elif os.path.exists('../steps/cvng/' + fileNameWithUnderscore):
+				updatedYamlData = read_yaml('../steps/cvng/' + fileNameWithUnderscore)
+				updateYaml('../steps/cvng/'+fileNameWithUnderscore,updatedYamlData)
+
+			elif os.path.exists('../steps/security/'+fileNameWithUnderscore):
+				updatedYamlData= read_yaml('../steps/security/' + fileNameWithUnderscore)
+				updateYaml('../steps/security/'+fileNameWithUnderscore,updatedYamlData)
 			else:
 				with open(directoryPath + fileNameWithUnderscore, 'w') as file:
 					yaml.dump(level[i], file, sort_keys=False)
+					print("calling read yaml for "+ fileNameWithUnderscore)
 					updatedYamlData = read_yaml(directoryPath + fileNameWithUnderscore)
 					file.close()
 					title={"title":fileName.removesuffix('.yaml')}
@@ -297,10 +330,10 @@ def my_function(module,prefix):
 
 
 
-
-moduleList = ['','custom','cd','ci','iacm','security','cvng']
+moduleList = ['iacm']
+# moduleList = ['']
 
 for module in moduleList:
 	print()
-	my_function(module,'StepNode')
+	my_function(module,'IACMStageNode')
 f.close()
